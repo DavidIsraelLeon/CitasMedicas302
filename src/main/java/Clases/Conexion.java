896098b8099aa3d5a0bdb294a9d44de72cbd6237
DavidIsraelLeon <dilggallardos@hotmail.com>
2020-07-15 23:27:50 -0500

    CitasMedicas302

---
+++ src/main/java/Clases/Conexion.java
@@ -0,0 +1,33 @@
+package Clases;
+
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+
+public class Conexion {
+public static Connection con;
+public static Statement state,state1;
+public static ResultSet result,result1;
+
+public void Conectar(String user,String pass)throws SQLException,ClassNotFoundException
+{  
+    
+
+            DriverManager.registerDriver( new com.mysql.cj.jdbc.Driver());
+            con = DriverManager.getConnection("jdbc:mysql://localhost/clinica", "root", "Juanfercho451408");
+          
+           
+
+    
+    state=con.createStatement(result.TYPE_SCROLL_SENSITIVE,result.CONCUR_UPDATABLE);
+    
+}
+ public static ResultSet consulta(String sql)throws SQLException{
+
+        state1 = (Statement) con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,result1.CONCUR_READ_ONLY);
+        result1 = state1.executeQuery(sql);//aqui hago consultas y devuelvo los resultados
+        return result1;
+}
+ 
+ 
+         }
